26db312d84c9900dd5d4ce3d5c8fdcf48eb186c7
gulshan-ara <gulshan3015@gmail.com>
2023-05-20 13:31:41 +0600

    feat - Splash screen and custom fonts are added to the project.

---
+++ App.js
@@ -0,0 +1,64 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { registerRootComponent } from "expo";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useState, useEffect } from "react";
+import * as Font from "expo-font";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          "black" : require("./assets/fonts/Roboto-Black.ttf")
+        });
+      } catch (error) {
+        console.log.error();
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+      <SafeAreaView>
+        <Text style={styles.label}>Hello!!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label : {
+    fontFamily: "black",
+    fontSize: 18, 
+    color: "black"
+  }
+});
+
+registerRootComponent(App);
