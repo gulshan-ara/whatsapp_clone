7c968c8769a5c1f05c5e4f0cbc36fa4b47a9fc12
gulshan-ara <gulshan3015@gmail.com>
2023-06-03 19:35:58 +0600

    style - styling the replyingTo component

--- components/ReplyTo.js
+++ components/ReplyTo.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import colors from "../constants/colors";
 
 const ReplyTo = ({ text, user, onCancel }) => {
 	const name = `${user.firstName} ${user.lastName}`;
@@ -17,4 +18,22 @@ const ReplyTo = ({ text, user, onCancel }) => {
 
 export default ReplyTo;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.extraLightGrey,
+    padding: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderLeftColor: colors.blue,
+    borderLeftWidth: 6,
+  },
+  textContainer: {
+    flex: 1,
+    marginRight: 5,
+  },
+  name:{
+    color: colors.blue,
+    fontFamily: 'medium',
+    letterSpacing: 0.3
+  }
+});
